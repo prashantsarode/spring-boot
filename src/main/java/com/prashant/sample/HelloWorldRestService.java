b7aec1931a1bade1d7bb4094690af121fba93225
Prashant Sarode <prashantsarode@gmail.com>
2017-03-03 21:01:43 -0600

    Adding simple rest service

--- src/main/java/com/prashant/sample/HelloWorldRestService.java
+++ src/main/java/com/prashant/sample/HelloWorldRestService.java
@@ -2,6 +2,7 @@ package com.prashant.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,10 @@ public class HelloWorldRestService {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldRestService.class, args);
 	}
-	
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!!!";
+
+	@RequestMapping("/{name}")
+	String home(@PathVariable String name) {
+		return name.isEmpty() ? "Hello World!!!" : "Hello " + name + "!!!";
 	}
-	
+
 }
